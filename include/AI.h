0f588374c08272254cfd9b3de6416f26a3ff764c
Kevin Siegall <digimonlord2@gmail.com>
2022-04-19 18:11:51 -0400

    Specified AI for 24

--- include/AI.h
+++ include/AI.h
@@ -1,13 +1,13 @@
-#include "BaseRobot.cpp"
+#include "r24/robot24.cpp"
 #include "GoalPosition.cpp"
-class AI{
+class AI24{
   public:
     // AI methods
-    void trackObjectsForCurrentFrame(BaseRobot *robot, vision *camera, std::vector<GoalPosition> &goals, int targetID = -1);
+    void trackObjectsForCurrentFrame(Robot24 *robot, vision *camera, std::vector<GoalPosition> &goals, int targetID = -1);
     int findGoalID(std::vector<GoalPosition> &goals);
-    int detectionAndStrafePhase(BaseRobot *robot, vision *camera, float *horizontalDistance, int matchStartTime);
+    int detectionAndStrafePhase(Robot24 *robot, vision *camera, float *horizontalDistance, int matchStartTime);
     float getDistanceFromArea(int area);
     float getDistanceFromWidth(int width);
-    void runAI(BaseRobot *robot, int matchStartTime);
+    void runAI(Robot24 *robot, int matchStartTime);
     GoalPosition* getGoalFromID(std::vector<GoalPosition> &goals, int targetID);
 };
