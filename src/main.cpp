e8ee70ce78ddf6532a02be8b038c7dd3e5614138
Ansel Chang <ansel.chang@yahoo.com>
2022-04-29 17:23:12 -0400

    erase auton selector

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,11 @@
 // INCLUDE RIGHT VERSION OF ROBOT (15 OR 24)
 // #include "main15.cpp"
-//#include "main24.cpp"
-#include "testAutoSelector.cpp"
-//#include "constants.h"
+#include "main24.cpp"
 
 using namespace vex;
 
 int main() {
 
   Competition.bStopAllTasksBetweenModes = true;
-  //Controller1->ButtonA.pressed(callback);
-  //return mainFunc();
-  wouldBeCalledMainFunctionButIsJustForTestingAutonSelector();
-  while (true) {
-    wait(20, msec);
-    Controller1->Screen.clearScreen();
-    Controller1->Screen.setCursor(1, 1);
-    Controller1->Screen.print("fuck %d", Controller1->Axis2.value());
-    
-  }
-  return 0;
+  return mainFunc();
 }
