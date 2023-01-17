5746907b143c224085567ad8dc1d3a9640e0e313
Ansel Chang <ansel.chang@yahoo.com>
2023-01-16 20:40:45 -0500

    test graph

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,25 @@
 // #include "main15.cpp"
 #include "main24.cpp"
 
+#include "Utility/VisualGraph.h"
+#include <time.h>
+#include <stdlib.h>
+
 using namespace vex;
 
 int main() {
 
   Competition.bStopAllTasksBetweenModes = true;
-  return mainFunc();
+  //return mainFunc();
+  srand(time(NULL));
+
+  VisualGraph g(0, 5, 6, 100);
+
+  while (true) {
+    int num = rand() % 5;
+    g.push(num);
+    g.display();
+    wait(20, vex::msec);
+  }
 
 }
